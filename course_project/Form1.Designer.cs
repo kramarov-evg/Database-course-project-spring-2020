3b6bb5f5773d512a2318fd09d1b585c421cf0cad
Kramarov Evgenii <kramarov-evg@mail.ru>
2020-05-13 02:31:19 +0300

    Project init

---
+++ course_project/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace course_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_btn = new System.Windows.Forms.Button();
+            this.login_edt = new System.Windows.Forms.TextBox();
+            this.pwd_edt = new System.Windows.Forms.TextBox();
+            this.login_lbl = new System.Windows.Forms.Label();
+            this.pwd_lbl = new System.Windows.Forms.Label();
+            this.signup_lnk = new System.Windows.Forms.LinkLabel();
+            this.signup_suggestion_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // login_btn
+            // 
+            this.login_btn.AutoSize = true;
+            this.login_btn.Location = new System.Drawing.Point(59, 90);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(59, 23);
+            this.login_btn.TabIndex = 2;
+            this.login_btn.Text = "Log In";
+            this.login_btn.UseVisualStyleBackColor = true;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // login_edt
+            // 
+            this.login_edt.Location = new System.Drawing.Point(12, 25);
+            this.login_edt.Name = "login_edt";
+            this.login_edt.Size = new System.Drawing.Size(156, 20);
+            this.login_edt.TabIndex = 0;
+            // 
+            // pwd_edt
+            // 
+            this.pwd_edt.Location = new System.Drawing.Point(12, 64);
+            this.pwd_edt.Name = "pwd_edt";
+            this.pwd_edt.PasswordChar = '•';
+            this.pwd_edt.Size = new System.Drawing.Size(156, 20);
+            this.pwd_edt.TabIndex = 1;
+            // 
+            // login_lbl
+            // 
+            this.login_lbl.Location = new System.Drawing.Point(12, 9);
+            this.login_lbl.Name = "login_lbl";
+            this.login_lbl.Size = new System.Drawing.Size(156, 13);
+            this.login_lbl.TabIndex = 6;
+            this.login_lbl.Text = "Login";
+            this.login_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pwd_lbl
+            // 
+            this.pwd_lbl.Location = new System.Drawing.Point(12, 48);
+            this.pwd_lbl.Name = "pwd_lbl";
+            this.pwd_lbl.Size = new System.Drawing.Size(156, 13);
+            this.pwd_lbl.TabIndex = 7;
+            this.pwd_lbl.Text = "Password";
+            this.pwd_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // signup_lnk
+            // 
+            this.signup_lnk.AutoSize = true;
+            this.signup_lnk.Location = new System.Drawing.Point(120, 120);
+            this.signup_lnk.Name = "signup_lnk";
+            this.signup_lnk.Size = new System.Drawing.Size(48, 13);
+            this.signup_lnk.TabIndex = 3;
+            this.signup_lnk.TabStop = true;
+            this.signup_lnk.Text = "Sign Up!";
+            this.signup_lnk.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // signup_suggestion_lbl
+            // 
+            this.signup_suggestion_lbl.AutoSize = true;
+            this.signup_suggestion_lbl.Location = new System.Drawing.Point(13, 120);
+            this.signup_suggestion_lbl.Name = "signup_suggestion_lbl";
+            this.signup_suggestion_lbl.Size = new System.Drawing.Size(59, 13);
+            this.signup_suggestion_lbl.TabIndex = 9;
+            this.signup_suggestion_lbl.Text = "New here?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 161);
+            this.Controls.Add(this.signup_suggestion_lbl);
+            this.Controls.Add(this.signup_lnk);
+            this.Controls.Add(this.pwd_lbl);
+            this.Controls.Add(this.login_lbl);
+            this.Controls.Add(this.pwd_edt);
+            this.Controls.Add(this.login_edt);
+            this.Controls.Add(this.login_btn);
+            this.MaximumSize = new System.Drawing.Size(200, 200);
+            this.MinimumSize = new System.Drawing.Size(200, 200);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.TextBox login_edt;
+        private System.Windows.Forms.TextBox pwd_edt;
+        private System.Windows.Forms.Label login_lbl;
+        private System.Windows.Forms.Label pwd_lbl;
+        private System.Windows.Forms.LinkLabel signup_lnk;
+        private System.Windows.Forms.Label signup_suggestion_lbl;
+        private OracleConnection connection;
+    }
+}
+
---
+++ course_project/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using Oracle.ManagedDataAccess.Client;
+
+namespace course_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.connection = GetDBConnection("127.0.0.1", 1521, "xe", "c##egor", "pass");
+        }
+
+        private static OracleConnection GetDBConnection(string host, int port, String sid, String user, String password)
+        {
+            string connString = "Data Source=(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = "
+                 + host + ")(PORT = " + port + "))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = "
+                 + sid + ")));Password=" + password + ";User ID=" + user;
+            
+            OracleConnection conn = new OracleConnection();
+
+            conn.ConnectionString = connString;
+
+            return conn;
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            String login = this.login_edt.Text;
+            String password = this.pwd_edt.Text;
+            String fetched_password = get_password(login);
+            if (password == fetched_password)
+            {
+                Console.WriteLine("FINE");
+            } else
+            {
+                // TODO: add Form for incorrect password
+            }
+        }
+
+        private String get_password(String login)
+        {
+            String query = "SELECT login, password FROM c##egor.users \"users\" WHERE \"users\".login = @login";
+            OracleCommand cmd = connection.CreateCommand();
+            cmd.CommandText = query;
+            OracleParameter login_param = cmd.Parameters.Add("@login", SqlDbType.VarChar);
+            login_param.Value = login;
+            String password = "";
+            this.connection.Open();
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    password = reader.GetString(0);
+                } else
+                {
+                    // TODO: add Form for user inexistance suggesting to sign up
+                }
+            }
+            return password;
+        }
+    }
+}
+
+
+//OracleConnection test = GetDBConnection("127.0.0.1", 1521, "xe", "c##egor", "pass");
+//test.Open();
+//string sql = "Select 42 from dual";
+//OracleCommand cmd = new OracleCommand();
+//cmd.Connection = test;
+//cmd.CommandText = sql;
+//using (DbDataReader reader = cmd.ExecuteReader())
+//{
+//    if (reader.HasRows)
+//    {
+
+//        while (reader.Read())
+//        {
+//            long empId = Convert.ToInt64(reader.GetValue(0));
+//            Console.WriteLine("empIdIndex:" + empId);
+//        }
+//    }
+//}
+//test.Close();
